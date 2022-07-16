b60501714b161471a1c245ff0c19357b69705ad8
DeviousLab <deviouslab@gmail.com>
2022-07-17 02:24:54 +0400

    Created test db and structure for jest tests
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,74 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 
-describe('AppController (e2e)', () => {
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+
+describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    app.close();
   });
+
+  describe('Auth', () => {
+    describe('Signup', () => {
+      
+    });
+
+    describe('Signiin', () => {
+
+    });
+  });
+
+  describe('User', () => {
+    describe('Get current user', () => {
+
+    });
+
+    describe('Edit current user', () => {
+
+    });
+  });
+
+  describe('Bookmark', () => {
+    describe('Create bookmark', () => {
+
+    });
+
+    describe('Get bookmarks', () => {
+
+    });
+
+    describe('Get bookmark by ID', () => {
+
+    });
+
+    describe('Edit bookmark', () => {
+
+    });
+
+    describe('Delete bookmark', () => {
+
+    });
+  });
+
+  it.todo('Should pass');
 });

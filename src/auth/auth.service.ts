97ee954f7951021fa93ac36559e8e306c63ab936
DeviousLab <deviouslab@gmail.com>
2022-07-16 04:22:27 +0400

    Integrated prisma orm
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 
 export class AuthService {
+  constructor(private prisma: PrismaService) {
+
+  }
+  
   signUp() {
     return "signUp";
   }
 
   signIn() {
-
+    return "signIn";
   }
 }
